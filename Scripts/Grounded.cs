e2586e0add9716350481ddb99b27af1abba87bd8
Haoxian Huang <hahuang8@gmail.com>
2017-10-07 00:21:24 -0700

    Add files via upload
    The scripts

---
+++ Scripts/Grounded.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour {
+    private Player player; // references the script player so we can acess Player
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnTrigEnter2d(Collider2D col)//get called when trigger enters something
+    {
+        player.grounded = true;
+    }
+
+    void OnTrigExit2d(Collider2D col)//when trigger exits something
+    {
+        player.grounded = false;
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 50f;
+    public float maxSpeed = 1;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start() {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();    //Acess rigid body of mcgregor?
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal"))); // Get speed and does stuff
+        if(Input.GetAxis("Horizontal")<-0.1f)
+            {
+            transform.localScale = new Vector3(-1, 1, 1);
+            }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");// The left arrow and right arrow A and D
+        //Moving the player
+        rb2d.AddForce((Vector2.right * speed) * h); //move the player spacebar
+        //limit speed of player
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);// If velocity.x is greater than 30 than we set it equal to vector2 maxspeed
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);// If velocity.x is greater than 30 than we set it equal to vector2 maxspeed
+        }
+
+    }
+}
